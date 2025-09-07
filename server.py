a83035cb1a4c7527062bbc4fa398695d86c685ed
Shadab Amir <shadabamir32@gmail.com>
2025-09-07 23:15:10 +0530

    Added custom tooling

--- server.py
+++ server.py
@@ -1,12 +1,11 @@
 from typing import Annotated
-
 from typing_extensions import TypedDict
-
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
-import os
+from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
-
+from langchain_tavily import TavilySearch
+from tools import BasicToolNode
 
 class State(TypedDict):
     # Messages have the type "list". The `add_messages` function
@@ -15,33 +14,68 @@ class State(TypedDict):
     messages: Annotated[list, add_messages]
 
 
+load_dotenv()
 graph_builder = StateGraph(State)
 
 
-os.environ["GOOGLE_API_KEY"] = "AIzaSyChMAh9azdtW1qOQYpI2jnLsptqG2-FPRQ"
+tool = TavilySearch(max_results=2)
+# tool.invoke("What's a 'node' in LangGraph?")
+tools = [tool]
 
 llm = init_chat_model("google_genai:gemini-2.0-flash")
+llm_with_tools = llm.bind_tools(tools)
+
+# TODO: Replace this with ToolNode from LangGraph
+tool_node = BasicToolNode(tools)
 
 def chatbot(state: State):
-    return {"messages": [llm.invoke(state["messages"])]}
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
 
+# Use prebuilt tool_condition function
+def route_tools(state: State):
+    """
+    Use in the conditional_edge to route to the ToolNode if the last message
+    has tool calls. Otherwise, route to the end.
+    """
+    if isinstance(state, list):
+        ai_message = state[-1]
+    elif messages := state.get("messages", []):
+        ai_message = messages[-1]
+    else:
+        raise ValueError(f"No messages found in input state to tool_edge: {state}")
+    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+        return "tools"
+    return END
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("tools", tool_node)
+# The `tools_condition` function returns "tools" if the chatbot asks to use a tool, and "END" if
+# it is fine directly responding. This conditional routing defines the main agent loop.
 # The first argument is the unique node name
 # The second argument is the function or object that will be called whenever
 # the node is used.
-graph_builder.add_node("chatbot", chatbot)
-
+graph_builder.add_conditional_edges(
+    "chatbot",
+    route_tools,
+    # The following dictionary lets you tell the graph to interpret the condition's outputs as a specific node
+    # It defaults to the identity function, but if you
+    # want to use a node named something else apart from "tools",
+    # You can update the value of the dictionary to something else
+    # e.g., "tools": "my_tools"
+    {"tools": "tools", END: END},
+)
+# Any time a tool is called, we return to the chatbot to decide the next step
+graph_builder.add_edge("tools", "chatbot")
 graph_builder.add_edge(START, "chatbot")
-
-graph_builder.add_edge("chatbot", END)
 graph = graph_builder.compile()
 
-from IPython.display import Image, display
+# from IPython.display import Image, display
 
-try:
-    display(Image(graph.get_graph().draw_mermaid_png()))
-except Exception:
-    # This requires some extra dependencies and is optional
-    pass
+# try:
+#     display(Image(graph.get_graph().draw_mermaid_png()))
+# except Exception:
+#     # This requires some extra dependencies and is optional
+#     pass
 
 def stream_graph_updates(user_input: str):
     for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
